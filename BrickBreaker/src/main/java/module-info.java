1f5c3bd485075811b8435a15a89521621ad38b74
Hunter Boyd <hboyd5310@gmail.com>
2026-03-29 23:13:35 -0700

    Delete BrickBreaker directory

--- BrickBreaker/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.brickbreaker {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.brickbreaker to javafx.fxml;
-    exports org.example.brickbreaker;
-}
--- BrickBreaker/src/main/java/org/example/brickbreaker/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.brickbreaker;
-
-import javafx.application.Application;
-
-public class Launcher {
-     static void main(String[] args) {
-        Application.launch(BrickBreaker.class, args);
-    }
-}
